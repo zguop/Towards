913f2b96441e216c2812741233b7e2d5ce4cd319
yubing <374735994@qq.com>
2019-08-16 16:43:53 +0800

    框架整理

---
+++ app/src/main/java/com/waitou/towards/model/main/fragment/CircleFragment.kt
@@ -0,0 +1,27 @@
+package com.waitou.towards.model.main.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.waitou.towards.R
+import com.waitou.wt_library.base.LazyFragment
+import kotlinx.android.synthetic.main.fragment_home.view.*
+
+/**
+ * Created by waitou on 16/12/23.
+ */
+
+class CircleFragment : LazyFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?): View {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.text.text = "ssss"
+    }
+}
